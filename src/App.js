f18e3f7623b90a6a7959f1a3eac50ce210333769
Anamgulzar67 <69452477+Anamgulzar67@users.noreply.github.com>
2023-09-14 14:29:42 +0500

    upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
-import Todo from "./components/Todo";
+import Covid from "./components/Covid";
 
-// import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <div>
-        {/* <Form /> */}
-        <Todo/>
-      </div>
+      <Covid/>
     </>
   );
 }
